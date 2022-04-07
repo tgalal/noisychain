7353bb2c4a89f716e8dca1c04e1091396c831400
Tarek Galal <tare2.galal@gmail.com>
2022-04-07 15:51:32 +0200

    [cli] support N protocol

--- noisychain-cli.py
+++ noisychain-cli.py
@@ -1,10 +1,7 @@
-import binascii
 from dissononce.extras.dh.experimental.secp256k1.public \
         import PublicKey
 from dissononce.extras.dh.experimental.secp256k1.private \
         import PrivateKey
-from dissononce.extras.dh.experimental.secp256k1.keypair \
-        import KeyPair
 from dissononce.extras.dh.experimental.secp256k1.secp256k1 \
         import SECP256K1DH
 from noisychain import ethutils
@@ -15,9 +12,10 @@ import asyncio
 import logging
 import noisychain
 from noisychain.protocols import MAGIC
-from noisychain.protocols import k
 from noisychain.protocols.k.initiator import KInitiatorProtocol
 from noisychain.protocols.k.responder import KResponderProtocol
+from noisychain.protocols.n.initiator import NInitiatorProtocol
+from noisychain.protocols.n.responder import NResponderProtocol
 from noisychain import channels
 from noisychain.ethutils import funding
 
@@ -31,9 +29,43 @@ noisychain-cli pubkey --key /path/to/key
 
 ADDRESS_EXAMPLES = ""
 PUBKEY_EXAMPLES = ""
-SEND_EXAMPLES = ""
+SEND_EXAMPLES = """
+Send Message Examples
+=====================
+
+> Send using K Protocol
+
+python noisychain-cli.py send \\
+        --protocol K \\
+        --key data/alice.bin --key-format hex \\
+        --pubkey 032c80af6ce6dbce65f0efd3f441dd2a8a5528a29d4d27483895a6bc17e1d5c240
+
+> Send using N Protocol
+
+python noisychain-cli.py send \\
+        --protocol N \\
+        --pubkey 032c80af6ce6dbce65f0efd3f441dd2a8a5528a29d4d27483895a6bc17e1d5c240
+
+"""
+RECV_EXAMPLES = """
+Receive Message Examples
+========================
+
+> Receive using K Protocol
+
+python noisychain-cli.py recv \\
+        --protocol K \\
+        --key data/bob.bin --key-format hex \\
+        --pubkey 037c52cf41d23397d6312cbcf8deba4b1ad7b68635de4d9d967c86e0496e3f4d1c
+
+> Receive using N Protocol
+
+python noisychain-cli.py recv \\
+        --protocol N \\
+        --key data/bob.bin --key-format hex
+
+"""
 CHANNEL_EXAMPLES = ""
-RECV_EXAMPLES = ""
 FUND_EXAMPLES = """
 Fund Examples
 =============
@@ -47,8 +79,10 @@ python noisychain-cli.py fund \\
 EXAMPLES = ()
 
 PROTOCOLS = {
-    "K" : (KInitiatorProtocol, KResponderProtocol)
+    "K" : (KInitiatorProtocol, KResponderProtocol),
+    "N" : (NInitiatorProtocol, NResponderProtocol),
 };
+ONEWAY_PROTOCOLS = ("K", "N")
 
 def read_data(source, fmt='raw', accept_stdin=False):
     if source == "-" and accept_stdin:
@@ -93,30 +127,46 @@ def handle_channel(args):
     channel = channels.derive_channel(local_static, remote_public, 0)
     print(channel)
 
-
 def handle_recv(args):
-    initiator = args.role == "initiator"
-    key = read_data(args.key, fmt=args.key_format)
     protocol_id = args.protocol
-    local_static = SECP256K1DH().generate_keypair(PrivateKey(key))
 
-    if args.address:
-        remote_public = asyncio.run(ethutils.address_to_public(args.address))
-    elif args.pubkey:
-        remote_public = PublicKey(binascii.unhexlify(args.pubkey))
+    if protocol_id in ONEWAY_PROTOCOLS:
+        assert args.role in ("responder", None)
+        initiator = False
     else:
-        print("Specify either an address or public key of the sender")
-        sys.exit(1)
+        initiator = args.role == "initiator"
+    key = read_data(args.key, fmt=args.key_format)
+    local_static = SECP256K1DH().generate_keypair(PrivateKey(key))
+    remote_public = None
+
+    # The following protocols require a remote static key
+    if protocol_id in ("K",):
+        if args.address:
+            remote_public = asyncio.run(ethutils.address_to_public(args.address))
+        elif args.pubkey:
+            remote_public = PublicKey(binascii.unhexlify(args.pubkey))
+        else:
+            print("Specify either an address or public key of the sender")
+            sys.exit(1)
+
+    Protocol = PROTOCOLS[protocol_id][0 if initiator else 1]
 
     if initiator:
-        pass
+        raise Exception("Not yet supported")
     else:
-        protocol = PROTOCOLS[protocol_id][1](local_static, remote_public)
-        protocol.setup()
+        address = ethutils.pubkey_to_address(local_static.public)
+        if protocol_id == "K":
+            assert remote_public, "Protocol requires specifing --pubkey"
+            protocol = Protocol(local_static, remote_public)
+            protocol.setup()
+            address = protocol.channel
+        elif protocol_id == "N":
+            protocol = Protocol(local_static)
+        else:
+            raise Exception("Unsupported")
 
-        channel = protocol.channel
         transactions = asyncio.run(
-                ethutils.get_transactions(to_address=channel))
+                ethutils.get_transactions(to_address=address))
 
         transactions_data = map(
                 lambda tx: binascii.unhexlify(tx.input[2:]),
@@ -132,10 +182,22 @@ def handle_recv(args):
             i += 1
 
 def handle_send(args):
-    initiator = args.role == "initiator"
-    key = read_data(args.key, fmt=args.key_format)
     protocol_id = args.protocol
-    local_static = SECP256K1DH().generate_keypair(PrivateKey(key))
+
+    if protocol_id in ONEWAY_PROTOCOLS:
+        assert args.role in ("initiator", None)
+        initiator = True
+    else:
+        initiator = args.role == "initiator"
+
+    if protocol_id in ("K",):
+        assert args.key, "Specified protocol requires passing --key KEY"
+        key = read_data(args.key, fmt=args.key_format)
+        local_static = SECP256K1DH().generate_keypair(PrivateKey(key))
+    else:
+        local_static = None
+    sys.stdout.write("Enter your message, followed by Ctrl+D > ")
+    sys.stdout.flush()
     message = sys.stdin.buffer.read().strip()
 
     if args.address:
@@ -147,9 +209,15 @@ def handle_send(args):
         sys.exit(1)
 
     if initiator:
-        protocol = PROTOCOLS[protocol_id][0](
-                local_static, remote_public, message)
-        protocol.setup()
+        Protocol = PROTOCOLS[protocol_id][0]
+        if protocol_id == "K":
+            protocol = Protocol(local_static, remote_public, message)
+            protocol.setup()
+        elif protocol_id == "N":
+            protocol = Protocol(remote_public, message)
+        else:
+            raise Exception(f"Unsupported protocol: {protocol_id}")
+
         print("\nSending...")
         txhash = asyncio.run(protocol.send())
         print(f"Sent: {txhash}")
@@ -215,7 +283,7 @@ if __name__ == '__main__':
 
     send_parser.add_argument('-k', '--key', action='store')
     send_parser.add_argument('-r', '--role', action='store',
-            choices=('initiator', 'responder'), required=True)
+            choices=('initiator', 'responder'))
     send_parser.add_argument('-p', '--protocol', action='store',
             choices=PROTOCOLS.keys(), required=True)
 
@@ -245,18 +313,18 @@ if __name__ == '__main__':
 
     recv_parser = subparsers.add_parser('recv',
             aliases=['r'],
-            epilog=SEND_EXAMPLES,
+            epilog=RECV_EXAMPLES,
             formatter_class=argparse.RawDescriptionHelpFormatter)
     recv_parser.set_defaults(func=handle_recv)
     recv_parser.add_argument('-d', '--debug', action="store_true")
 
     recv_parser.add_argument('-k', '--key', action='store', required=True)
     recv_parser.add_argument('-r', '--role', action='store',
-            choices=('initiator', 'responder'), required=True)
+            choices=('initiator', 'responder'), required=False)
     recv_parser.add_argument('-p', '--protocol', action='store',
             choices=PROTOCOLS.keys(), required=True)
 
-    group = recv_parser.add_mutually_exclusive_group(required=True)
+    group = recv_parser.add_mutually_exclusive_group(required=False)
     group.add_argument('-K', '--pubkey', action='store')
     group.add_argument('-a', '--address', action='store')
 
